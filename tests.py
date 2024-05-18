a7645c544acd0bf0f2106b398d801c329c775499
Matthew Smith <smitmat3@oregonstate.edu>
2024-05-18 14:09:57 -0700

    Added new test class for function 1 along with random tests

--- tests.py
+++ tests.py
@@ -1,10 +1,51 @@
 import unittest
+from task import my_datetime
+from datetime import datetime
+import random
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
+
+class Function2Tests(unittest.TestCase):
+    # Citation:
+    # For utcfromtimestamp(): https://docs.python.org/3/library/datetime.html#datetime.datetime.utcfromtimestamp
+    # For strftime(): https://docs.python.org/3/library/datetime.html#datetime.date.strftime
+
+    def test1(self):
+        """Test edge case of 0 seconds"""
+        num_sec = 0
+        actual = my_datetime(num_sec)
+        expected = datetime.utcfromtimestamp(num_sec).strftime('%m-%d-%Y')
+        self.assertEqual(actual, expected)
+
+    def test2(self):
+        """Test example in assignment"""
+        num_sec = 987654321
+        actual = my_datetime(num_sec)
+        expected = datetime.utcfromtimestamp(num_sec).strftime('%m-%d-%Y')
+        self.assertEqual(actual, expected)
+
+    def xtest_random_large_set(self):
+        """Random tests that go up to 12/31/9999"""
+        tests = 10000
+        for i in range(tests):
+            num_sec = random.randint(0, 253402300799)
+            actual = my_datetime(num_sec)
+            expected = datetime.utcfromtimestamp(num_sec).strftime('%m-%d-%Y')
+            self.assertEqual(actual, expected, msg='num_secs={}'.format(num_sec))
+
+    def test_random_small_set(self):
+        """Random tests that can run locally but don't go up to year 9999"""
+        tests = 10000
+        for i in range(tests):
+            num_sec = random.randint(0, 9999999999)
+            actual = my_datetime(num_sec)
+            expected = datetime.utcfromtimestamp(num_sec).strftime('%m-%d-%Y')
+            self.assertEqual(actual, expected, msg='num_secs={}'.format(num_sec))
 
 
 if __name__ == '__main__':
